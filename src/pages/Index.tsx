63324c0a2fddda9920e78380ff3968105f751e31
M-Manohar-Reddy30 <manoharreddyind@gmail.com>
2026-01-04 12:58:27 +0530

    Fix ReviewsPreviewSection filename typo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import AboutSection from '@/components/home/AboutSection';
 import SpacesSection from '@/components/home/SpacesSection';
 import MenuPreviewSection from '@/components/home/MenuPreviewSection';
 import GalleryPreviewSection from '@/components/home/GalleryPreviewSection';
-import ReviewsPreviewSection from '@/components/home/ReviewsPreviewSection';
+import ReviewsPreviewSection from "@/components/home/ReviewsPreviewSection";
 import LocationSection from '@/components/home/LocationSection';
 
 const Index = () => {
